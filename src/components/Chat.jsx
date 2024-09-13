7cb142f96937b7dda15189ce803244fe2388caaf
Reuben Frith <reubenfrith007@gmail.com>
2024-09-13 18:08:36 +1000

    Add LeftPanelToolbar and Chat components
    - Added LeftPanelToolbar component to the project
    - Added Chat component with a chat window, text input field, and send button

---
+++ src/components/Chat.jsx
@@ -0,0 +1,25 @@
+// Functional React component for the chat window
+
+const Chat = () => {
+  // Component should have a window showing the chat, a text input field in the bottom, and a button to send the message
+
+  return (
+    <div>
+      <div>
+        <div>
+          <div>
+            <p>Message 1</p>
+            <p>Message 2</p>
+            <p>Message 3</p>
+          </div>
+        </div>
+        <div>
+          <input type="text" />
+          <button>Send</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Chat
